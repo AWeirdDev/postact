2e29a5745dfcdf57df939e82fc2d062a42c13416
AWeirdDev <aweirdscratcher@gmail.com>
2025-12-26 22:57:43 +0800

    fixed naming

--- src/utilities/css.ts
+++ src/utilities/css.ts
@@ -1,7 +1,27 @@
+function getName(key: string): string {
+  let text = "";
+  let idx = 0;
+
+  while (idx < key.length) {
+    const char = key[idx];
+
+    // isUpperCase
+    if (char == char.toUpperCase()) {
+      text += "-" + char.toLowerCase();
+    } else {
+      text += char;
+    }
+  }
+
+  if (text.startsWith("webkit")) {
+    return "-" + text;
+  } else {
+    return text;
+  }
+}
+
 export function css(styles: Partial<CSSStyleDeclaration>): string;
-
 export function css(tsa: TemplateStringsArray, ...args: string[]): string;
-
 export function css(
   arg0: Partial<CSSStyleDeclaration> | TemplateStringsArray,
   ...args: string[]
@@ -12,7 +32,7 @@ export function css(
       .trim();
   } else {
     return Object.entries(arg0).reduce(
-      (acc, [k, v]) => acc + `${k}: ${v};`,
+      (acc, [k, v]) => acc + `${getName(k)}: ${v};`,
       "",
     );
   }
