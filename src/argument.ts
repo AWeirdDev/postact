1eed5ecd6438baf03c50655e6c324e469bbcc812
AWeirdDev <aweirdscratcher@gmail.com>
2026-01-16 15:12:46 +0800

    updated types for html

--- src/argument.ts
+++ src/argument.ts
@@ -2,6 +2,29 @@ import { isPrimitive } from "./utilities";
 import { isSubscribable, type Subscribable } from "./subscribable";
 
 import type { VirtualItem } from "./vdom/structure";
+import type { Component, ComponentInstance } from "./component";
+
+type EventMap = {
+  animation: AnimationEvent;
+  clipboard: ClipboardEvent;
+  composition: CompositionEvent;
+  drag: DragEvent;
+  focus: FocusEvent;
+  input: InputEvent;
+  keyboard: KeyboardEvent;
+  mouse: MouseEvent;
+  touch: TouchEvent;
+  pointer: PointerEvent;
+  toggle: ToggleEvent;
+  transition: TransitionEvent;
+  ui: UIEvent;
+  wheel: WheelEvent;
+  generic: Event;
+};
+
+type EventHandler = {
+  [K in keyof EventMap]: (e: EventMap[K]) => any;
+}[keyof EventMap];
 
 export type Argument =
   | null
@@ -12,7 +35,9 @@ export type Argument =
   | string
   | Subscribable<any>
   | VirtualItem
-  | Function;
+  | EventHandler
+  | Component<{}>
+  | ComponentInstance;
 
 export enum ArgumentType {
   Empty,
