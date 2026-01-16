b05f1286246a95dd55b28828890dce00fcef1726
AWeirdDev <aweirdscratcher@gmail.com>
2026-01-16 23:46:30 +0800

    wtf

--- packages/postact-jsx/test.tsx
+++
@@ -1,8 +0,0 @@
-import { state, select } from "@postact/core";
-
-function Counter() {
-  const $count = state(0);
-  return <button onclick={() => $count.update((v) => v + 1)}>Current: {$count}</button>;
-}
-
-select("#app").render(<Counter />);
