abd8d01331bf5864b86777a3149045af68ed5a48
AWeirdDev <aweirdscratcher@gmail.com>
2026-01-27 15:54:20 +0800

    client impl

---
+++ packages/ssr/src/client.ts
@@ -0,0 +1,37 @@
+import { deserialize, t } from "@postact/serde";
+
+import { Transmitable, TransmitWriter } from "./shared";
+
+interface CallActionParams<T, R> {
+  ident: string;
+  paramsSchema: t.SchemaOfType<T>;
+  params: T;
+  returnSchema: t.SchemaOfType<R>;
+}
+
+/**
+ * **(client-side)** `@postact/serde` client.
+ */
+export class SerdeClient {
+  #url: string;
+
+  constructor(url: string) {
+    this.#url = url;
+  }
+
+  async callAction<T, R>({ ident, paramsSchema, params, returnSchema }: CallActionParams<T, R>): Promise<R> {
+    const writer = new TransmitWriter(Transmitable.EventType.Action);
+    writer.placeAction(ident, paramsSchema, params);
+
+    const result = await fetch(this.#url, {
+      method: "POST",
+      body: writer.finish(),
+    });
+    if (!result.ok) throw new Error(`failed, got response with status ${result.status}`);
+
+    const streamReader = result.body?.getReader()!;
+    const content = await streamReader.read();
+
+    return deserialize(returnSchema, content.value!.buffer);
+  }
+}
